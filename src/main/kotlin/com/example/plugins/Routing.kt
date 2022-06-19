d39d84142ecda923bce32f43a94e91aa83546073
kamilchulakov <mr1english4@gmail.com>
2022-06-20 02:32:48 +0300

    Release

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,20 +1,14 @@
 package com.example.plugins
 
 import io.ktor.server.routing.*
-import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
-import java.io.File
 
 fun Application.configureRouting() {
 
     routing {
         static("/") {
-            staticRootFolder = File("front")
-            file("index.html")
-            default("index.html")
+            resources("files")
         }
     }
 }
